65c7451aa30a0caf8da9223ec32fc68996b79a2f
mayra <m.frias09@hotmail.com>
2025-01-13 12:36:18 -0300

    m

--- express/src/Services/ProductoService.js
+++ express/src/Services/ProductoService.js
@@ -1,32 +1,28 @@
 import ProductosModel from '../models/Product.js'
 const getAllWithFilters = async ({ limit = 10, page = 1, query = {}, sort = 'asc' }) => {
     try {
-        // Aseguramos que los parámetros sean válidos
+        
         const parsedLimit = isNaN(limit) ? 10 : parseInt(limit);
         const parsedPage = isNaN(page) ? 1 : parseInt(page);
         const parsedSort = sort === 'desc' ? -1 : 1;
 
         const skip = (parsedPage - 1) * parsedLimit;
 
-        // Realizar la búsqueda con paginación, filtros y ordenamiento
         const products = await ProductosModel.find(query)
             .skip(skip)
             .limit(parsedLimit)
             .sort({ price: parsedSort });
 
-        // Contar los productos para paginación
         const totalProducts = await ProductosModel.countDocuments(query);
 
-        // Calcular la cantidad total de páginas
+     
         const totalPages = Math.ceil(totalProducts / parsedLimit);
 
-        // Definir la paginación
         const hasPrevPage = parsedPage > 1;
         const hasNextPage = parsedPage < totalPages;
         const prevPage = hasPrevPage ? parsedPage - 1 : null;
         const nextPage = hasNextPage ? parsedPage + 1 : null;
 
-        // Preparar las respuestas con paginación y enlaces
         return {
             status: 'success',
             payload: products,
